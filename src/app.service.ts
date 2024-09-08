aebfda2a0695f222958ca589d166a68c9b915e8a
Truong Son <truongsonpt.80@gmail.com>
2024-09-08 11:19:28 +0700

    up

---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): any {
+    return {
+      id: 1,
+      name: 'truong son',
+      pro: 'vercel',
+    };
+  }
+}
